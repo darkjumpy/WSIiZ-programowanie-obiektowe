0d0bb038a92a3a05fe03730242fdf2dbe54a2c9a
Jakub Kołodziejczyk <darkjumpy@gmail.com>
2023-10-25 13:16:22 +0200

    Initial commit

---
+++ APP/Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Lab1.Constructions;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var constr = new Construction(66, 66, 66, 66, "test");
+        }
+    }
+}
